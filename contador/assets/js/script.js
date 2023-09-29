c888c674aaf44a35d5d41a0731176a08152435f7
DH_Pedro <98625506+DHPedro@users.noreply.github.com>
2023-09-29 12:52:09 -0300

    Add files via upload

---
+++ contador/assets/js/script.js
@@ -0,0 +1,13 @@
+
+    var numerointeiro = document.getElementById("numero");
+    var numero = 0;
+
+    function menos() {
+        numero = numero - 1;
+        numerointeiro.innerHTML = numero;
+    }
+
+    function mais() {
+        numero = numero + 1;
+        numerointeiro.innerHTML = numero;
+    }
